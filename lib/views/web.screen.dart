14acc561cf8af1539b19301519b0a7b31f83a384
qwaiks <kwaku315@gmail.com>
2022-03-22 13:47:43 +0000

    remove old portfolio view

--- lib/views/web.screen.dart
+++ lib/views/web.screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_webapp/config/size_config.dart';
 import 'package:portfolio_webapp/config/utils.dart';
-import 'package:portfolio_webapp/views/sections/about.section.dart';
-import 'package:portfolio_webapp/views/sections/project.section.dart';
-import 'package:portfolio_webapp/views/sections/top.section.dart';
-import 'package:portfolio_webapp/views/sections/work.section.dart';
+
 
 class WebScreen extends StatelessWidget {
   const WebScreen({Key key}) : super(key: key);
@@ -17,19 +14,7 @@ class WebScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       body: ListView(
         children: [
-          Align(
-              alignment: Alignment.topRight,
-              child: SizedBox(
-                width: getProportionateScreenWidth(1241),
-                height: getProportionateScreenHeight(769),
-                child: TopSection(),
-              )),
-          Utils.verticalSpacer(space: 100),
-          WorkSection(),
-          Utils.verticalSpacer(space: 100),
-          ProjectSection(),
-          Utils.verticalSpacer(space: 100),
-          AboutSection(),
+
         ],
       ),
     );
