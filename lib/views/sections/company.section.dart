0e70ba8b46a56a3c2a60f599ac2cbc007538bbe3
qwaiks <kwaku315@gmail.com>
2022-03-23 09:39:39 +0000

    work section added to view,

---
+++ lib/views/sections/company.section.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
+
+class CompanySection extends StatelessWidget {
+  const CompanySection({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.7,
+      child: ,
+    );
+  }
+}
---
+++ lib/views/sections/work.section.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_webapp/config/theme.dart';
+import 'package:portfolio_webapp/config/utils.dart';
+
+import '../../config/constants.dart';
+
+class WorkSection extends StatelessWidget {
+  const WorkSection({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Align(
+          alignment: Alignment.centerRight,
+          child: Text(
+            'Recent Work',
+            style: primaryTextTheme.headlineLarge,
+          ),
+        ),
+        const Divider(
+          color: Colors.white,
+          thickness: 3,
+        ),
+        Utils.verticalSpacer(),
+        GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              childAspectRatio: 0.8,
+              crossAxisCount: 2,
+              crossAxisSpacing: 20,
+            ),
+            itemCount: 5,
+            itemBuilder: (ctx, index) {
+              return WorkTile(
+                paddingTop: index.isOdd ? 100 : 0,
+                image: AppImages.waCommImage,
+                title: 'GrainMate Mobile',
+                desc:
+                    'GrainMate app is supercharging African farmers and agribusinesses with the tools and resources they need to get to the next level ⚡️',
+              );
+            })
+      ],
+    );
+  }
+}
+
+class WorkTile extends StatelessWidget {
+  final String image;
+  final String title;
+  final String desc;
+  final String androidLink;
+  final String iosLink;
+  final double paddingTop;
+
+  const WorkTile(
+      {Key key,
+      this.image,
+      this.paddingTop,
+      this.title,
+      this.androidLink,
+      this.desc,
+      this.iosLink})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 70, right: 70, top: paddingTop),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(image),
+          Utils.verticalSpacer(),
+          Text(
+            title,
+            style: primaryTextTheme.titleLarge,
+          ),
+          Utils.verticalSpacer(),
+          Text(
+            desc,
+            style: primaryTextTheme.bodyLarge,
+          )
+        ],
+      ),
+    );
+  }
+}
