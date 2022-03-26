de32fd9f9de555f01afc0239a169cbfce4979862
qwaiks <kwaku315@gmail.com>
2022-03-26 15:41:17 +0000

    update constants and themes

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -5,12 +5,25 @@ class AppConstants {
   static const satoshiFont = "Satoshi";
 }
 
-class AppImages{
+const double displayLargeWeb = 75;
+const double displayLargeMobile = 40;
+const double headlineLargeWeb = 70;
+const double headlineLargeMobile = 32;
+const double headlineMediumWeb = 65;
+const double headlineMediumMobile = 28;
+const double titleLargeWeb = 50;
+const double titleLargeMobile = 24;
+const double bodyLargeWeb = 16;
+const double bodyLargeMobile = 16;
+const double bodyMediumWeb = 14;
+const double bodyMediumMobile = 14;
+
+class AppImages {
   static const portfolioImage = "assets/images/portfolio_image.jpg";
   static const waCommImage = "assets/images/wacomm_img.png";
 }
 
-class DummyData{
+class DummyData {
   static const companies = [
     'BroadSpectrum',
     'Walulel',
@@ -22,12 +35,25 @@ class DummyData{
 }
 
 class AppString {
+  static const name = 'Kwaku Owusu-Ansa';
+  static String jobTitle = 'Mobile Engineer \nbased in Accra,\nGhana';
+
+  static String professionalDesc = 'With over 3 years experience in building'
+      ' digital products that users love and cherish, I have worked mostly with '
+      'startups in Food tech, Agri-tech, Health, Fin-tech, Social Media & Education.';
+
+  static String professionalDescCont = 'I am the founder of UX Campus: An Non '
+      'Profit that seeks to inspire people to fall in love with UX Design, by '
+      'organising free 1-2 day workshops across Ghana.';
+
   static const welcomeName = "Hello,\nI'm Kwaku\nNtim";
   static const welcomeDesc =
       "I design and build beautiful mobile, desktop & web apps & experiences";
   static const portfolio = "PORTFOLIO";
   static const contact = "CONTACT ME";
   static const resume = "RESUME";
+
+
 }
 
 class Api {
